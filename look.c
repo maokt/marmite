ebc036dfd7d331f9fcbb9a60055843a160ed4a11
Marty Pauley <marty@martian.org>
2015-09-16 22:26:41 +0900

    set a sensible font (for use in Japan)

--- look.c
+++ look.c
@@ -2,8 +2,12 @@
 #include <gdk/gdk.h>
 #include "palette.h"
 
+static PangoFontDescription *font;
+
 void marmite_look(VteTerminal *vte) {
     vte_terminal_set_colors_rgba(vte, &palette[palette_fg], &palette[palette_bg], palette, palette_size);
     vte_terminal_set_cursor_shape(vte, VTE_CURSOR_SHAPE_IBEAM);
+    font = pango_font_description_from_string("VL Gothic 16");
+    vte_terminal_set_font(vte, font);
 }
 
