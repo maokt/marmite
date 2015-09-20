10bdc57ec76c8cdd069880e351957baf3f82f233
Marty Pauley <marty@martian.org>
2015-09-20 17:21:18 +0900

    probably should be const

--- make-palette.c
+++ make-palette.c
@@ -32,16 +32,16 @@ static const char * const palette[] = {
 int main(void) {
     size_t n = sizeof palette / sizeof palette[0];
     printf("#ifndef _solarized_palette\n#define _solarized_palette 1\n");
-    printf("static GdkRGBA palette[%zu] = {\n", n);
-    for (size_t i = 0; i < n; ++i) {
+    printf("static const GdkRGBA palette[%zu] = {\n", n);
+    for (int i = 0; i < n; ++i) {
         GdkRGBA colour;
         gdk_rgba_parse(&colour, palette[i]);
         printf("    { %0.17f, %0.17f, %0.17f, %0.2f },\n", colour.red, colour.green, colour.blue, colour.alpha);
     }
     printf("};\n");
-    printf("static size_t palette_size = %zu;\n", n);
-    printf("static size_t palette_fg = %d;\n", 12);
-    printf("static size_t palette_bg = %d;\n", 8);
+    printf("static const size_t palette_size = %zu;\n", n);
+    printf("static const int palette_fg = %d;\n", 12);
+    printf("static const int palette_bg = %d;\n", 8);
     printf("#endif\n");
 }
 
