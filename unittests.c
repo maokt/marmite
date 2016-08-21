ed4291141a25333fdf2156ac43c287ec0b0ea24c
Marty Pauley <marty@martian.org>
2016-08-21 20:29:42 +0900

    test default config

--- unittests.c
+++ unittests.c
@@ -2,15 +2,18 @@
 #include <locale.h>
 #include "config.h"
 
-static void dummy(void) {
-  g_assert_cmpint(2, ==, 2);
-  g_assert_cmpint(3, ==, 3);
+static void default_config(void) {
+    char *argv[1] = { "test" };
+    MarmiteConfig *cfg = marmite_config((sizeof argv / sizeof argv[0]), argv, NULL);
+    g_assert_nonnull(cfg);
+    g_assert_true(g_str_has_suffix(cfg->command[0], "sh"));
+    g_assert_cmpstr(cfg->font, ==, "VL Gothic 16");
 }
 
 int main (int argc, char *argv[]) {
-  setlocale (LC_ALL, "");
-  g_test_init(&argc, &argv, NULL);
-  g_test_add_func("/marmite/dummy", dummy);
-  return g_test_run ();
+    setlocale (LC_ALL, "");
+    g_test_init(&argc, &argv, NULL);
+    g_test_add_func("/marmite/config/default", default_config);
+    return g_test_run ();
 }
 
