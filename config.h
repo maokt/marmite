09c1d42f7a76bc3786f280506410c39319fe082a
Marty Pauley <marty@martian.org>
2016-08-24 23:45:01 +0900

    include just once

--- config.h
+++ config.h
@@ -1,6 +1,11 @@
+#ifndef _marmite_config
+#define _marmite_config 1
+
 typedef struct MarmiteConfig {
     char *font;
     char **command;
 } MarmiteConfig;
 
 MarmiteConfig *marmite_config(int argc, char *argv[], GOptionGroup *extra_options);
+
+#endif
